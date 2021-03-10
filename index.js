dc51c2013a63c5c311bbea2ee3a3abb88993d809
Thiago-Cez <thiago.silva.ts804@gmail.com>
2021-03-10 15:32:38 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const api = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/'
+});
+
+const listaUsuarios = [
+    'Ivens',
+    'Jailson',
+    'Maria',
+    'Natalia',
+    'Ariel',
+    'Nykolle'
+]
+
+// Query Params
+// Ex: localhost:3000/usuarios?nome=Ivens
+app.get('/usuarios', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = listaUsuarios.filter(i => i.includes(nome || ''));
+    return res.json(listaRetorno);
+});
+
+// Route Params 
+// Ex: ecommer.com.br/produtos/4/detalhes
+app.get('/hello/:usuario', (req, res) => {    
+    const { usuario } = req.params;
+    return res.send(`Hello world ${usuario}`);
+});
+
+app.get('/pokemons', async (req, res) => {
+    const responseAxios = await api.get('pokemon?limit=100&offset=200');
+    const { data } = responseAxios;
+    const retorno = {
+        ...data,
+        results: data.results.map(item => {
+            item.greetings = 'Olá';
+            return item;
+        })
+    }
+    return res.json(retorno);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
